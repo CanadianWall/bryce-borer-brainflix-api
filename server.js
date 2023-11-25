9a3d9bf65f3f1ee83ef99b2925d30733189a0f32
Bryce Borer <bryce.borer@gmail.com>
2023-11-25 16:46:00 -0500

    DELETE is now operational!

--- server.js
+++ server.js
@@ -11,72 +11,15 @@ const { PORT, CORS_ORIGIN } = process.env
 app.use(cors({ PORT, CORS_ORIGIN }))
 
 
-// console.log(videoData.videos)
-// console.log(videoData.videoDetails[0].comments)
-// console.log(PORT)
-
-
-let students = [
-  {
-    id: '5dbb3dd8-49d8-4c3b-93d9-59230836b2f1',
-    name: 'Brynhildr Sadler',
-    program: 'Web Dev',
-    grade: 75,
-  },
-  {
-    id: '594daba3-e1e4-4a5f-89ae-372dfb95a16d',
-    name: 'Joan Leon',
-    program: 'UX',
-    grade: 72,
-  },
-  {
-    id: '9c05b68b-e51b-42ce-8b89-10f8aa32db2b',
-    name: 'Mark Summers',
-    program: 'Web Dev',
-    grade: 87,
-  },
-  {
-    id: '5c49d6a0-74a2-4202-af90-204ef0e35fbc',
-    name: 'Tanja Zawisza',
-    program: 'Web Dev',
-    grade: 92,
-  },
-  {
-    id: '23e91f79-f553-4f4f-88ac-09070cb38ac1',
-    name: 'Slavomir Amato',
-    program: 'UX',
-    grade: 78,
-  },
-  {
-    id: '1c4b7c42-23af-4168-aced-f87cb02f5172',
-    name: 'Tihana Anand',
-    program: 'UX',
-    grade: 60,
-  },
-  {
-    id: '86a81142-b6dc-422b-9d55-7bbc04aa012c',
-    name: 'Reima Ivov',
-    program: 'Web Dev',
-    grade: 95,
-  },
-  {
-    id: '8d8b7afa-8896-44cb-9bb7-bc21beda4fd5',
-    name: 'Demokritos Shafir',
-    program: 'UX',
-    grade: 83,
-  },
-];
-
 // GET is working!
 app.get('/videos', (req, res) => {
   res.json(videoData.videos);
 })
 
-
 // Working on POST
-app.post('/videos/:id/comments', (req, res) => {
+app.post('/videos/:videoId/comments', (req, res) => {
   const comment = req.body.comment;
-  const videoId = req.params.id;
+  const videoId = req.params.videoId;
 
   const newComment = {
     id: uuidv4(),
@@ -93,25 +36,36 @@ app.post('/videos/:id/comments', (req, res) => {
   //Adds new comment to the proper video
   readVideos.videoDetails[newCommentVideoIndex].comments = [...readVideos.videoDetails[newCommentVideoIndex].comments, newComment]
 
-  console.log("newThingId: ", readVideos.videoDetails[newCommentVideoIndex].comments)
-
   fs.writeFile("./data/videos2.json",
     JSON.stringify(readVideos), () => {
       console.log("videos2.json has been created!")
       res.json(newComment);
     });
 
-  // read file and latest entry
-
 });
 
-app.delete('/api/v1/students/:id', (req, res) => {
-  const studentIdParam = req.params.id
-  const filteredStudents = students.filter((student) => {
-    return student.id !== studentIdParam
+app.delete('/videos/:videoId/comments/:commentId', (req, res) => {
+  const videoId = req.params.videoId
+  const commentId = req.params.commentId
+
+  const getVideos = () => fs.readFileSync("./data/videos2.json", { endoding: 'utf8' })
+  let readVideos = JSON.parse(getVideos())
+  const videoIndex = readVideos.videoDetails.findIndex((e) => e.id == videoId)
+
+  const filteredVideoComments = readVideos.videoDetails[videoIndex].comments.filter((comment) => {
+    console.log("comment.id: ", comment.id)
+    return comment.id !== commentId;
   });
-  students = filteredStudents
-  res.send(filteredStudents)
+
+ //Updates comments with deleted comment removed
+ readVideos.videoDetails[videoIndex].comments = filteredVideoComments
+ 
+
+ fs.writeFile("./data/videos2.json",
+   JSON.stringify(readVideos), () => {
+     console.log("videos2.json has been created!")
+     res.json(filteredVideoComments);
+   });
 
 });
 
